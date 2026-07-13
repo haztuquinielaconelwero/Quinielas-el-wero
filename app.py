86c609aca8d1811c0f640b80c839ba3919501691
Irving Gonzalez <haztuquinielaconelwero@gmail.com>
2026-07-13 00:29:18 -0600

    Cambios en la app

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ def crear_tablas():
 
         conn.commit()
 # ── Esto de abajo trabaja con la informacion de la Jornada ───────────────────────────────────────────────────────────────────────────────────────────────
-JORNADA_ACTUAL = "Jornada 1"
+JORNADA_ACTUAL = "Jornada 2"
 PARTIDOS = [
     {
         "id": 1,

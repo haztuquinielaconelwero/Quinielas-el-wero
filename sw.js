ab038c7fa4f8e502cd83c7036b7b5f2a633eb5a2
Irving Gonzalez <haztuquinielaconelwero@gmail.com>
2026-08-06 14:54:17 -0600

    Cambios en el sw

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ══════════════════════════════════════      Service Worker — Quinielas El Wero                   ════════════════════════════════════            //
-const VERSION = "v1.3.0";
+const VERSION = "v1.3.1";
 const CACHE_ESTATICO = `estatico-${VERSION}`;
 const CACHE_IMAGENES = `imagenes-${VERSION}`;
 const ARCHIVOS_PRECARGA = [
